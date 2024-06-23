6a141f23c300b436f91c2931ce19e39c418d0e16
alalaradi <alaradi@gmail.com>
2024-06-23 17:14:25 +0300

    4

---
+++ mutability.js
@@ -0,0 +1,19 @@
+
+// const person = {
+//     name: 'Rick',
+//     age: 77,
+//     country: 'US',
+// }
+
+const clone1 = Object.assign({}, person); // will not work if there is a property with the type of 'object', use '_.cloneDeep(obj)'.
+const clone2 = Object.assign({}, person);
+
+const samePerson = person;
+
+person.age++;
+person.country = 'FR';
+console.log(person);
+
+console.log(clone1);
+console.log(clone2);
+console.log(samePerson);
