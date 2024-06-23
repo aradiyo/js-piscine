e9e3c7cde167e594ce94c1b509a94259d5cf7545
alalaradi <alaradi@gmail.com>
2024-06-23 17:03:26 +0300

    1

--- circular.js
+++
@@ -1,7 +0,0 @@
-/*
-Create an object named circular that has a property named circular with itself as the value.
-*/
-const circular = {};
-circular.circular = circular;
-
-console.log(circular);
--- concat-str.js
+++
@@ -1,9 +0,0 @@
-/*
-Create a function named concatStr which takes 2 arguments and concatenates them.
-*/
-
-const concatStr = (str1, str2) => `${str1}${str2}`;
-
-
-console.log(concatStr('hello', 'world'));
-
