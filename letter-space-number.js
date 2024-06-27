bae77dbcafb6250316cfc7cbf10381b7de66ef0e
Ali Alaradi <alaradi@gmail.com>
2024-06-27 06:30:45 +0000

    find

--- letter-space-number.js
+++ letter-space-number.js
@@ -1,5 +1,5 @@
 const letterSpaceNumber = (str) => {
- let arr = str.match(/\w \d(?=\w|$)/g);
- if (arr == null){return [];}
- else {return arr;}
+    let arr= str.match(/\w \d(?=\W|$)/g);
+    if (arr=== null){return [];}
+    else {return arr;}
 }
